5ea9db0f76d3073ca5ad2b5e023fb5d36fc3937c
adeltahenti <adel.tahenti@citycable.ch>
2018-08-31 08:52:23 +0200

    Use get to fetch users from external API

--- js/app.js
+++ js/app.js
@@ -1,33 +1,33 @@
 const http = new EasyHTTP();
 
 // Get Users
-// http.get('https://jsonplaceholder.typicode.com/users')
-//   .then(data => {
-//     console.log(data);
-//     let output = '';
-//     data.forEach(user => {
-//       output += `
-//         <li class="list-group-item">
-//           <ul class="list-group">
-//             <li class="list-group-item"><strong>Id : </strong>${user.id}</li>
-//             <li class="list-group-item bg-light"><strong>Name : </strong>${user.name}</li>
-//             <li class="list-group-item"><strong>Username : </strong>${user.username}</li>
-//             <li class="list-group-item"><strong>Email : </strong>${user.email}</li>
-//           </ul>
-//         </li>
-//       `;
-//     });
+http.get('https://jsonplaceholder.typicode.com/users')
+  .then(data => {
+    console.log(data);
+    let output = '';
+    data.forEach(user => {
+      output += `
+        <li class="list-group-item">
+          <ul class="list-group">
+            <li class="list-group-item"><strong>Id : </strong>${user.id}</li>
+            <li class="list-group-item bg-light"><strong>Name : </strong>${user.name}</li>
+            <li class="list-group-item"><strong>Username : </strong>${user.username}</li>
+            <li class="list-group-item"><strong>Email : </strong>${user.email}</li>
+          </ul>
+        </li>
+      `;
+    });
 
-//     document.getElementById('output').innerHTML = output;
-//   })
-//   .catch(err => console.log(err));
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(err => console.log(err));
 
 // User data
-const data = {
-  name: 'John Doe',
-  username: 'johndoe',
-  email: 'jdoe@gmail.com'
-}
+// const data = {
+//   name: 'John Doe',
+//   username: 'johndoe',
+//   email: 'jdoe@gmail.com'
+// }
 
 // Create User
 // http.post('https://jsonplaceholder.typicode.com/users', data)
@@ -40,6 +40,6 @@ const data = {
 //   .catch(err => console.log(err));
 
 // Delete User
-http.delete('https://jsonplaceholder.typicode.com/users/2')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+// http.delete('https://jsonplaceholder.typicode.com/users/2')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
